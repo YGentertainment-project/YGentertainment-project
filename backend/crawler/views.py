4e1593ad05ddd2c180f6c0eadee6166fab05e962
minhee <minheekim3@naver.com>
2021-12-27 17:07:27 +0900

    daily_read API

--- backend/crawler/views.py
+++ backend/crawler/views.py
@@ -12,6 +12,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from crawler.models import Socialblade
 import json
+import datetime
 
 # process = CrawlerProcess(get_project_settings())
 
@@ -65,5 +66,64 @@ def show_data(request):
         for queryset_value in platform_queryset_values:
             platform_datas.append(queryset_value)
         return JsonResponse(data={'success': True, 'data': platform_datas})
+    else:
+        return JsonResponse(status=400, data={'success': False})
+
+
+#daily read API
+@csrf_exempt
+@require_http_methods(['GET'])  # only get and post
+def daily_read(request):
+    platform = request.GET.get('platform', None)
+    type = request.GET.get('type', None)
+    start_date = request.GET.get('start_date', None)
+    end_date = request.GET.get('end_date', None)
+
+    if type=="누적":
+        start_date_dateobject = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+        filter_objects = Socialblade.objects.filter(platform=platform, recorded_date__year=start_date_dateobject.year,
+             recorded_date__month=start_date_dateobject.month, recorded_date__day=start_date_dateobject.day)
+        if filter_objects.exists():
+            filter_objects_values = filter_objects.values()
+            filter_datas = []
+            for filter_value in filter_objects_values:
+                filter_datas.append(filter_value)
+            return JsonResponse(data={'success': True, 'data': filter_datas})
+        else:
+            return JsonResponse(status=400, data={'success': True, 'data': []})
+    # elif type=="기간별"://기간별에 속하는 모든 data 전송
+    #     start_date_dateobject = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S').date()
+    #     end_date_dateobject = datetime.datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S').date()
+    #     db_start_date = datetime.datetime.combine(start_date_dateobject, datetime.time.min)
+    #     db_end_date = datetime.datetime.combine(end_date_dateobject, datetime.time.max) #change to 23:59:59
+    #     filter_objects = Socialblade.objects.filter(platform=platform, recorded_date__range=(db_start_date,db_end_date))
+    #     if filter_objects.exists():
+    #         filter_objects_values = filter_objects.values()
+    #         filter_datas = []
+    #         for filter_value in filter_objects_values:
+    #             filter_datas.append(filter_value)
+    #         return JsonResponse(data={'success': True, 'data': filter_datas})
+    #     else:
+    #         return JsonResponse(status=400, data={'success': True, 'data': []})
+    elif type=="기간별":
+        # 전날 값을 구함
+        start_date_dateobject = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S').date() - datetime.timedelta(1)
+        end_date_dateobject = datetime.datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S').date()
+        filter_objects_start = Socialblade.objects.filter(platform=platform, recorded_date__year=start_date_dateobject.year,
+             recorded_date__month=start_date_dateobject.month, recorded_date__day=start_date_dateobject.day)
+        filter_objects_end = Socialblade.objects.filter(platform=platform, recorded_date__year=end_date_dateobject.year,
+             recorded_date__month=end_date_dateobject.month, recorded_date__day=end_date_dateobject.day)
+        filter_datas_start = []
+        filter_datas_end = []
+        if filter_objects_start.exists():
+            filter_objects_start_values = filter_objects_start.values()
+            for filter_value in filter_objects_start_values:
+                filter_datas_start.append(filter_value)
+        if filter_objects_end.exists():
+            filter_objects_end_values = filter_objects_end.values()
+            filter_datas_end = []
+            for filter_value in filter_objects_end_values:
+                filter_datas_end.append(filter_value)
+        return JsonResponse(data={'success': True, 'data': {'start':filter_datas_start, 'end':filter_datas_end}})
     else:
         return JsonResponse(status=400, data={'success': False})
