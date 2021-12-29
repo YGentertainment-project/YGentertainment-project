d06690e874b8915034c5ffef6c6c9f917efc457d
soomin9106 <soomin9106@naver.com>
2021-12-29 11:46:44 +0900

    change to dataprocess app 2

--- backend/config/views.py
+++ backend/config/views.py
@@ -6,15 +6,3 @@ from rest_framework.renderers import HTMLFormRenderer, TemplateHTMLRenderer
 
 # Create your views here.
 
-def base(request):
-    values = {
-      'first_depth' : '데이터 리포트',
-    }
-    return render(request, 'config/main.html',values)
-
-def daily(request):
-    values = {
-      'first_depth' : '데이터 리포트',
-      'second_depth': '시간별 리포트'
-    }
-    return render(request, 'config/daily.html',values)
