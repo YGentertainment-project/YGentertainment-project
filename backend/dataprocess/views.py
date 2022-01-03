a9f9bbc61ed6020a4e19f0e50972044e7d73ceaf
soomin9106 <soomin9106@naver.com>
2022-01-03 22:11:04 +0900

    change to class API in artist page & modify view

--- backend/dataprocess/views.py
+++ backend/dataprocess/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.urls.base import set_script_prefix
 from rest_framework.renderers import HTMLFormRenderer, TemplateHTMLRenderer
 from config.serializers import CollectTargetItemSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 def base(request):
@@ -26,7 +28,6 @@ def platform(request):
     }
     return render(request, 'dataprocess/platform.html',values)
 
-<<<<<<< HEAD
 def artist(request):
     artists = Artist.objects.all()
     values = {
@@ -36,6 +37,7 @@ def artist(request):
     }
     return render(request, 'dataprocess/artist.html',values)
 
+@csrf_exempt
 def artist_add(request):
     platforms = Platform.objects.all()
     values = {
@@ -45,10 +47,7 @@ def artist_add(request):
     }
     return render(request, 'dataprocess/artist_add.html',values)
 
-from rest_framework import viewsets
-=======
->>>>>>> 6d9bc31... change API to classview
-from rest_framework.views import APIView
+#from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 from django.http.response import JsonResponse
@@ -56,10 +55,11 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
 from config.models import CollectTargetItem
-from utils.decorators import login_required
+#from utils.decorators import login_required
 from utils.api import APIView, validate_serializer
 
 #======platform=======
+
 class PlatformAPI(APIView):
     # @login_required
     def get(self, request):
@@ -130,53 +130,7 @@ class PlatformAPI(APIView):
         except:
             return JsonResponse(data={'success': False}, status=400)
 
-<<<<<<< HEAD
-#======artist=======
-#artist read
-@csrf_exempt
-@require_http_methods(['GET'])  # only get
-def artist_read(request):
-    try:
-        artist_objects = Artist.objects.all()
-        if artist_objects.exists():
-            artist_objects_values = artist_objects.values()
-            artist_datas = []
-            for artist_value in artist_objects_values:
-                artist_datas.append(artist_value)
-            return JsonResponse(data={'success': True, 'data': artist_datas})
-        else:
-            return JsonResponse(data={'success': True, 'data': []})
-    except:
-        return JsonResponse(status=400, data={'success': False})
 
-#artist-platform read
-@csrf_exempt
-@require_http_methods(['GET'])  # only get
-def platforms_of_artist_read(request):
-    try:
-        artist = request.GET.get('artist', None)
-        # 해당 artist 찾기
-        artist_object = Artist.objects.filter(name = artist)
-        artist_object = artist_object.values()[0]
-        # 해당 artist를 가지는 collect_target들 가져오기
-        collecttarget_objects = CollectTarget.objects.filter(artist_id=artist_object['id'])
-        if collecttarget_objects.exists():
-            collecttarget_objects_values = collecttarget_objects.values()
-            platform_datas = []
-            for collecttarget_value in collecttarget_objects_values:
-                platform_object = Platform.objects.get(pk=collecttarget_value['platform_id'])
-                platform_datas.append({
-                    'artist_id':artist_object['id'],
-                    'id': collecttarget_value['id'],
-                    'name': platform_object.name,
-                    'target_url':collecttarget_value['target_url']
-                })
-            return JsonResponse(data={'success': True, 'data': platform_datas})
-        else:
-            return JsonResponse(data={'success': True, 'data': []})
-    except:
-        return JsonResponse(status=400, data={'success': False})
-=======
 class ArtistAPI(APIView):
     # @login_required
     def get(self, request):
@@ -195,7 +149,6 @@ class ArtistAPI(APIView):
                 return JsonResponse(data={'success': True, 'data': []})
         except:
             return JsonResponse(status=400, data={'success': False})
->>>>>>> 6d9bc31... change API to classview
 
     # @login_required
     def post(self, request):
@@ -211,11 +164,8 @@ class ArtistAPI(APIView):
                 # 2. 현재 존재하는 모든 platform에 대해 collect_target 생성 -> artist와 연결
                 platform_objects = Platform.objects.all()
                 platform_objects_values = platform_objects.values()
-                for platform_objects_value in platform_objects_values:
-                    if artist_object['target_urls'].get(platform_objects_value['name']):
-                        platform_target_url = artist_object['target_urls'][platform_objects_value['name']]
-                    else:
-                        platform_target_url = ""
+                for i,platform_objects_value in enumerate(platform_objects_values):
+                    platform_target_url = artist_object['urls'][i]
                     collecttarget = CollectTarget(
                         platform_id = platform_objects_value['id'],
                         artist_id = artist_serializer.data['id'],
@@ -245,36 +195,6 @@ class ArtistAPI(APIView):
         except:
             return JsonResponse(data={'success': False}, status=400)
 
-<<<<<<< HEAD
-
-#artist-platform-collecttargetitem read
-@csrf_exempt
-@require_http_methods(['GET'])  # only get
-def collecttargetitems_read(request):
-    try:
-        artist = request.GET.get('artist', None)
-        platform = request.GET.get('platform', None)
-        # 해당 artist,platform 찾기
-        artist_object = Artist.objects.filter(id = artist)
-        artist_object = artist_object.values()[0]
-        platform_object = Platform.objects.filter(name = platform)
-        platform_object = platform_object.values()[0]
-        # 해당 artist와 platform을 가지는 collect_target 가져오기
-        collecttarget_objects = CollectTarget.objects.filter(artist_id=artist_object['id'], platform_id = platform_object['id'])
-        if collecttarget_objects.exists():
-            collecttargetitems_datas = []
-            collecttarget_objects_value = collecttarget_objects.values()[0]
-            collecttargetitmes_objects = CollectTargetItem.objects.filter(collect_target_id=collecttarget_objects_value['id'])
-            collecttargetitmes_values = collecttargetitmes_objects.values()
-            for collecttargetitmes_value in collecttargetitmes_values:
-                collecttargetitems_datas.append(collecttargetitmes_value)
-            return JsonResponse(data={'success': True, 'data': collecttargetitems_datas})
-        else:
-            return JsonResponse(data={'success': True, 'data': []})
-    except:
-        return JsonResponse(status=400, data={'success': False})
-=======
->>>>>>> 6d9bc31... change API to classview
 
 class PlatformOfArtistAPI(APIView):
     # @login_required
@@ -295,6 +215,7 @@ class PlatformOfArtistAPI(APIView):
                 for collecttarget_value in collecttarget_objects_values:
                     platform_object = Platform.objects.get(pk=collecttarget_value['platform_id'])
                     platform_datas.append({
+                        'artist_id':artist_object['id'],
                         'id': collecttarget_value['id'],
                         'name': platform_object.name,
                         'target_url':collecttarget_value['target_url']
@@ -305,33 +226,6 @@ class PlatformOfArtistAPI(APIView):
         except:
             return JsonResponse(status=400, data={'success': False})
 
-<<<<<<< HEAD
-#artist create
-@csrf_exempt
-@require_http_methods(['POST'])  # only post
-def artist_create(request):
-    try:
-        artist_object = JSONParser().parse(request)
-        artist_serializer = ArtistSerializer(data=artist_object)
-        if artist_serializer.is_valid():
-            # 1. artist 생성
-            artist_serializer.save()
-            # 2. 현재 존재하는 모든 platform에 대해 collect_target 생성 -> artist와 연결
-            platform_objects = Platform.objects.all()
-            platform_objects_values = platform_objects.values()
-            for index,platform_objects_value in enumerate(platform_objects_values):
-                platform_target_url = artist_object['urls'][index]
-                collecttarget = CollectTarget(
-                    platform_id = platform_objects_value['id'],
-                    artist_id = artist_serializer.data['id'],
-                    target_url = platform_target_url
-                )
-                collecttarget.save()
-            return JsonResponse(data={'success': True, 'data': artist_serializer.data}, status=status.HTTP_201_CREATED)
-        return JsonResponse(data={'success': False,'data': artist_serializer.errors }, status=400)
-    except:
-        return JsonResponse(data={'success': False}, status=400)
-=======
     # @login_required
     def put(self, request):
         """
@@ -346,7 +240,6 @@ def artist_create(request):
             return JsonResponse(data={'success': True}, status=status.HTTP_201_CREATED)
         except:
             return JsonResponse(data={'success': False}, status=400)
->>>>>>> 6d9bc31... change API to classview
 
 
 class CollectTargetItemAPI(APIView):
@@ -359,7 +252,7 @@ class CollectTargetItemAPI(APIView):
             artist = request.GET.get('artist', None)
             platform = request.GET.get('platform', None)
             # 해당 artist,platform 찾기
-            artist_object = Artist.objects.filter(name = artist)
+            artist_object = Artist.objects.filter(id = artist)
             artist_object = artist_object.values()[0]
             platform_object = Platform.objects.filter(name = platform)
             platform_object = platform_object.values()[0]
