f7004b1986bb27f52c312697c375afd5c6353d6a
cyw320712 <cyw7515@naver.com>
2022-01-03 14:21:57 +0900

    add logger setting

--- backend/yg/settings.py
+++ backend/yg/settings.py
@@ -51,6 +51,7 @@ LOCAL_APPS = [
     'crawler',
     'dataprocess',
     'corsheaders',
+    'utils',
 ]
 
 INSTALLED_APPS = VENDOR_APPS + LOCAL_APPS
@@ -130,6 +131,46 @@ STATICFILES_DIRS = [
 LOG_PATH = os.path.join(DATA_DIR, "log")
 
 LOGGING_HANDLERS = ['console']
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '[%(asctime)s] - [%(levelname)s] - [%(name)s:%(lineno)d]  - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': LOG_PATH,
+            'formatter': 'standard',z``
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': LOGGING_HANDLERS,
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django.db.backends': {
+            'handlers': LOGGING_HANDLERS,
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        '': {
+            'handlers': LOGGING_HANDLERS,
+            'level': 'WARNING',
+            'propagate': True,
+        }
+    },
+}
 
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
