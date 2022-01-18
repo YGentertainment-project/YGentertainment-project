e65030855d61066d2870f89c90cb2ec02a323247
kingh2160 <sunrinkingh23160@gmail.com>
2022-01-19 05:19:37 +0900

    [Update] URL DB에서 받아오도록 수정

--- backend/crawler/scrapy_app/spiders/socialblade_tiktok.py
+++ backend/crawler/scrapy_app/spiders/socialblade_tiktok.py
@@ -1,6 +1,9 @@
 import scrapy
 from urllib.parse import urlparse
 from ..items import SocialbladeTiktokItem
+from dataprocess.models import CollectTarget
+from dataprocess.models import Artist
+from dataprocess.models import Platform
 
 SOCIALBLADE_DOMAIN = "socialblade.com"
 SOCIALBLADE_ROBOT = "https://socialblade.com/robots.txt"
@@ -14,77 +17,14 @@ class TiktokSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        artist_url = {
-            "(여자)아이들": "https://socialblade.com/tiktok/user/official_gidle",
-            "강다니엘": "https://socialblade.com/tiktok/user/konnect_kangdaniel",
-            "뉴이스트": "https://socialblade.com/tiktok/user/nuest_official",
-            "레드벨벳": "https://socialblade.com/tiktok/user/redvelvet_smtown",
-            "레드벨벳 아이린": "",
-            "레드벨벳 슬기": "",
-            "레드벨벳 조이": "",
-            "레드벨벳 웬디": "",
-            "레드벨벳 예리": "",
-            "마마무": "https://socialblade.com/tiktok/user/official_mamamoo",
-            "마마무 화사": "",
-            "몬스타엑스": "https://socialblade.com/tiktok/user/monsta_x_514",
-            "방탄소년단": "https://socialblade.com/tiktok/user/bts_official_bighit",
-            "BLACKPINK": "https://socialblade.com/tiktok/user/bp_tiktok",
-            "BLACKPINK 로제": "https://socialblade.com/tiktok/user/roses_are_rosie",
-            "BLACKPINK 리사": "",
-            "BLACKPINK 제니": "",
-            "BLACKPINK 지수": "",
-            "BIGBANG": "",
-            "BIGBANG 지드래곤": "",
-            "샤이니": "",
-            "세븐틴": "https://socialblade.com/tiktok/user/seventeen17_official",
-            "슈퍼주니어": "https://socialblade.com/tiktok/user/superjunior_smtown",
-            "스트레이키즈": "https://socialblade.com/tiktok/user/jypestraykids",
-            "아스트로": "https://socialblade.com/tiktok/user/astro_official",
-            "아스트로 차은우": "",
-            "에스파": "https://socialblade.com/tiktok/user/aespa_official",
-            "WINNER": "https://socialblade.com/tiktok/user/wn_tiktok",
-            "크래비티": "https://socialblade.com/tiktok/user/cravityofficial",
-            "트와이스": "https://socialblade.com/tiktok/user/twice_tiktok_official",
-            "AB6IX": "https://socialblade.com/tiktok/user/ab6ix.official",
-            "에이티즈": "https://socialblade.com/tiktok/user/ateez_official_",
-            "엔하이픈": "https://socialblade.com/tiktok/user/enhypen",
-            "EXO": "",
-            "엑소 백현": "",
-            "IKON": "https://socialblade.com/tiktok/user/ikon_tiktok",
-            "ITZY": "https://socialblade.com/tiktok/user/itzyofficial",
-            "NCT": "https://socialblade.com/tiktok/user/official_nct",
-            "NCT 127": "",
-            "NCT DREAM": "",
-            "NCT U": "",
-            "더보이즈": "https://socialblade.com/tiktok/user/creker_theboyz",
-            "TREASURE": "https://socialblade.com/tiktok/user/yg_treasure_tiktok",
-            "TXT": "https://socialblade.com/tiktok/user/txt.bighitent",
-            "WayV": "https://socialblade.com/tiktok/user/official_wayv",
-            "청하": "https://socialblade.com/tiktok/user/official_chungha",
-            "선미": "https://socialblade.com/tiktok/user/official_sunmi",
-            "전소미": "https://socialblade.com/tiktok/user/somi_official_",
-            "AKMU": "",
-            "AKMU 이수현": "https://socialblade.com/tiktok/user/akmu_suhyun",
-            "아이유": "",
-            "헤이즈": "",
-            "소녀시대": "",
-            "소녀시대 태연": "",
-            "이하이": "",
-            "정은지": "",
-            "젝스키스": "",
-            "백예린": "",
-            "볼빨간사춘기": "",
-            "현아": "https://socialblade.com/tiktok/user/hyunaofficial",
-            "비투비": "https://socialblade.com/tiktok/user/official_btob",
-            "SF9": "https://socialblade.com/tiktok/user/sf9official",
-            "스테이씨": "https://socialblade.com/tiktok/user/stayc_official",
-            "오마이걸": "https://socialblade.com/tiktok/user/wm_ohmygirl",
-            "위클리": "https://socialblade.com/tiktok/user/weeekly",
-            "HYBE LABELS": "",
-            "SM ENTERTAINMENT": "",
-            "JYP ENTERTAINMENT": "",
-        }
-        for artist, url in artist_url.items():
+        crawl_url = {}
+        tiktok_platform_id = Platform.objects.get(name='tiktok').id
+        CrawlingTarget = CollectTarget.objects.filter(platform_id=tiktok_platform_id)
+        for row in CrawlingTarget:
+            artist_name = Artist.objects.get(id=row.artist_id).name
+            artist_url = row.target_url
+            crawl_url[artist_name] = artist_url
+        for artist, url in crawl_url.items():
             print("artist : {}, url : {}, url_len: {}".format(
                 artist, url, len(url)))
             if len(url) > 0:
@@ -109,8 +49,8 @@ class TiktokSpider(scrapy.Spider):
         else:
             item = SocialbladeTiktokItem()
             item['artist'] = artist
-            item['uploads'] = uploads.replace(',', '')
-            item['followers'] = followers.replace(',', '')
-            item['likes'] = likes.replace(',', '')
+            item['uploads'] = 0 if not uploads else uploads.replace(',', '')
+            item['followers'] = 0 if not followers else followers.replace(',', '')
+            item['likes'] = 0 if not likes else likes.replace(',', '')
             item['url'] = response.url
             yield item
--- backend/crawler/scrapy_app/spiders/socialblade_youtube.py
+++ backend/crawler/scrapy_app/spiders/socialblade_youtube.py
@@ -1,6 +1,10 @@
 import scrapy
 from urllib.parse import urlparse
+
 from ..items import SocialbladeYoutubeItem
+from dataprocess.models import CollectTarget
+from dataprocess.models import Artist
+from dataprocess.models import Platform
 
 SOCIALBLADE_DOMAIN = "socialblade.com"
 YOUTUBE_DOMAIN = "youtube.com"
@@ -17,77 +21,15 @@ class YoutubeSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        artist_url = {
-            "(여자)아이들": "https://socialblade.com/youtube/channel/UCritGVo7pLJLUS8wEu32vow",
-            "강다니엘": "https://socialblade.com/youtube/channel/UCHGJwrjlY6rmZJInxRmiztA",
-            "뉴이스트": "https://socialblade.com/youtube/user/pledisnuest",
-            "레드벨벳": "https://socialblade.com/youtube/channel/UCk9GmdlDTBfgGRb7vXeRMoQ",
-            "레드벨벳 아이린": "",
-            "레드벨벳 슬기": "",
-            "레드벨벳 조이": "",
-            "레드벨벳 웬디": "",
-            "레드벨벳 예리": "",
-            "마마무": "https://socialblade.com/youtube/user/wamamamoo",
-            "마마무 화사": "",
-            "몬스타엑스": "https://socialblade.com/youtube/channel/UCZvCP6sWj75MwpUP4LVtpNw",
-            "방탄소년단": "https://socialblade.com/youtube/user/bangtantv",
-            "BLACKPINK": "https://socialblade.com/youtube/channel/UCOmHUn--16B90oW2L6FRR3A",
-            "BLACKPINK 로제": "https://socialblade.com/youtube/channel/UCBo1hnzxV9rz3WVsv__Rn1g",
-            "BLACKPINK 리사": "https://socialblade.com/youtube/channel/UC35HKvKYPkri4Grd5KXl3wQ",
-            "BLACKPINK 제니": "https://socialblade.com/youtube/channel/UCNYi_zGmR519r5gYdOKLTjQ",
-            "BLACKPINK 지수": "",
-            "BIGBANG": "https://socialblade.com/youtube/user/bigbang",
-            "BIGBANG 지드래곤": "https://socialblade.com/youtube/user/officialgdragon",
-            "샤이니": "https://socialblade.com/youtube/user/shinee",
-            "세븐틴": "https://socialblade.com/youtube/user/pledis17",
-            "슈퍼주니어": "https://socialblade.com/youtube/user/superjunior",
-            "스트레이키즈": "https://socialblade.com/youtube/user/superjunior",
-            "아스트로": "https://socialblade.com/youtube/channel/UCZqY2yIsAM9wh3vvMwKd27g",
-            "아스트로 차은우": "",
-            "에스파": "https://socialblade.com/youtube/channel/UC9GtSLeksfK4yuJ_g1lgQbg",
-            "WINNER": "https://socialblade.com/youtube/user/officialygwinner",
-            "크래비티": "https://socialblade.com/youtube/channel/UCRm-0JVuUFh5HV7NGG7qXlQ",
-            "트와이스": "https://socialblade.com/youtube/channel/UCzgxx_DM2Dcb9Y1spb9mUJA",
-            "AB6IX": "https://socialblade.com/youtube/channel/UCwytna6i1rFCt13a3Qvuk-A",
-            "에이티즈": "https://socialblade.com/youtube/channel/UC2e4Ukj5Pfr7cb3KpJAFBdQ",
-            "엔하이픈": "https://socialblade.com/youtube/channel/UCArLZtok93cO5R9RI4_Y5Jw",
-            "EXO": "https://socialblade.com/youtube/user/exok",
-            "엑소 백현": "https://socialblade.com/youtube/channel/UCUyr5000laFgF79tWJB3rXQ",
-            "iKON": "https://socialblade.com/youtube/user/officialikon",
-            "ITZY": "https://socialblade.com/youtube/channel/UCDhM2k2Cua-JdobAh5moMFg",
-            "NCT": "https://socialblade.com/youtube/channel/UCwgtORdDtUKhpjE1VBv6XfA",
-            "NCT 127": "https://socialblade.com/youtube/channel/UCk2E0dbAyEJWnrN2bbQOcbg",
-            "NCT DREAM": "https://socialblade.com/youtube/channel/UCXURHJRGr4-EB3l87kcbElw",
-            "NCT U": "",
-            "더보이즈": "https://socialblade.com/youtube/channel/UCkJ1rbOrsyPfBuHNfnLPm-Q",
-            "TREASURE": "https://socialblade.com/youtube/channel/UCx9hXYOCvUYwrprEqe4ZQHA",
-            "TXT": "https://socialblade.com/youtube/channel/UCtiObj3CsEAdNU6ZPWDsddQ",
-            "WayV": "https://socialblade.com/youtube/channel/UC-ZHt5Zgadfx-B1CM63Lqew",
-            "청하": "https://www.youtube.com/c/CHUNGHA_OFFICIAL/about",
-            "선미": "https://socialblade.com/youtube/channel/UCsVpgRB8YHLWA0ZrkhtHvTA",
-            "전소미": "",
-            "AKMU": "https://socialblade.com/youtube/user/officialakmu",
-            "AKMU 이수현": "https://socialblade.com/youtube/channel/UCOiM8FuCUFJkuUjCmB14rgg",
-            "아이유": "https://socialblade.com/youtube/channel/UC3SyT4_WLHzN7JmHQwKQZww",
-            "헤이즈": "https://socialblade.com/youtube/channel/UCsXigGjbC_l4ttk-oahTfVg",
-            "소녀시대": "https://socialblade.com/youtube/user/girlsgeneration",
-            "소녀시대 태연": "https://socialblade.com/youtube/channel/UC5z2fxN6rs69cSyXur6X6Mg",
-            "이하이": "https://socialblade.com/youtube/user/officialleehi",
-            "정은지": "https://socialblade.com/youtube/channel/UCHTZl9wrDAV45L8IPS7ZYzg",
-            "젝스키스": "https://socialblade.com/youtube/channel/UCcADqTjMyMol8B8mWm9n6rA",
-            "백예린": "https://socialblade.com/youtube/channel/UCYoNPLKd1kXh4v3bvIx1pTA",
-            "볼빨간사춘기": "https://www.youtube.com/c/BOL4OFFICIAL/about",
-            "현아": "https://socialblade.com/youtube/channel/UC0uTcuuOtUFwtn9aKUVGjXg",
-            "비투비": "https://socialblade.com/youtube/user/officialbtob",
-            "SF9": "https://socialblade.com/youtube/channel/UC8HNshpReWjQv1WpwzhPHjA",
-            "스테이씨": "https://www.youtube.com/c/STAYC/about",
-            "오마이걸": "https://socialblade.com/youtube/channel/UC-qYkzKFdekoEniRu_FS3zg",
-            "위클리": "https://socialblade.com/youtube/c/weeekly",
-            "HYBE LABELS": "https://socialblade.com/youtube/c/hybelabels",
-            "SM ENTERTAINMENT": "https://socialblade.com/youtube/user/smtown",
-            "JYP ENTERTAINMENT": "https://socialblade.com/youtube/user/jypentertainment",
-        }
-        for artist, url in artist_url.items():
+        crawl_url = {}
+        youtube_platform_id = Platform.objects.get(name='youtube').id
+        CrawlingTarget = CollectTarget.objects.filter(platform_id=youtube_platform_id)
+        for row in CrawlingTarget:
+            artist_name = Artist.objects.get(id=row.artist_id).name
+            artist_url = row.target_url
+            crawl_url[artist_name] = artist_url
+
+        for artist, url in crawl_url.items():
             print("artist : {}, url : {}, url_len: {}".format(
                 artist, url, len(url)))
             if len(url) > 0:
