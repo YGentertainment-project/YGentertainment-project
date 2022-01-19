c362485297e58fe955d1a258a30cfb78657385e1
cyw <cyw7515@naver.com>
2022-01-19 17:03:01 +0900

    [Edit] celery.py rabbitmq IP

--- backend/crawler/celery.py
+++ backend/crawler/celery.py
@@ -8,14 +8,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yg.settings')
 
 production_env = get_env("YG_ENV", "dev") == "production"
 if production_env:
-    app = Celery('crawler', backend='rpc://', broker='amqp://guest:guest@172.18.0.1:5672/')
+    app = Celery('crawler', backend='rpc://', broker='amqp://guest:guest@yg-rabbitmq:5672/')
 else:
     app = Celery('crawler', backend='rpc://', broker='amqp://guest:guest@localhost:5672/')
 
 # settigs.py에서 celery setting을 CELERY_로 시작하게 한다.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
 
 app.conf.update(
     task_serializer='json',
