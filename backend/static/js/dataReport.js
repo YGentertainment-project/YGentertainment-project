53a2af61c26ecddc9cc4c8e0a8ce75204fecb918
minhee <minheekim3@naver.com>
2022-01-24 18:26:30 +0900

    excel functions modify

--- backend/static/js/dataReport.js
+++ backend/static/js/dataReport.js
@@ -867,3 +867,7 @@ $('input[name=end_date]').hide()
 $('input[name=day]').hide()
 $('input[name=week]').hide()
 $('input[name=month]').hide()
+
+if(document.getElementById('alert') != null){
+    alert(ocument.getElementById('alert').innerHTML);
+}
