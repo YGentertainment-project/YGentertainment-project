bdc78a51d8023e29d27f93ea4e27153cbbace7c0
Yangseungchan <uvzone@naver.com>
2022-02-13 18:10:13 +0900

    [Fix] Spotify log error

--- backend/crawler/scrapy_app/spiders/spotify.py
+++ backend/crawler/scrapy_app/spiders/spotify.py
@@ -34,7 +34,7 @@ class SpotifySpider(scrapy.Spider):
             self.crawl_logger.error(f"[400], {artist}, spotify, {url}")
             # Xpath Error라고 나올 경우, 잘못된 Xpath 형식으로 생긴 문제입니다.
         if listen is None or follow is None:
-            self.crawl_logger.error(f"[400] {artist}, spotify, {url}")
+            self.crawl_logger.error(f"[400], {artist}, spotify, {url}")
             # Xpath가 오류여서 해당 페이지에서 element를 찾을 수 없는 경우입니다.
             # 혹은, Xpath에는 문제가 없으나 해당 페이지의 Element가 없는 경우입니다.
             # 오류일 경우 item을 yield 하지 않아야 합니다.
