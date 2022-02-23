b2365b0488f6d7e16e1f3727c061bbc9fd4c4201
minhee <minheekim3@naver.com>
2022-02-23 23:20:54 +0900

    Delete platform_target_item

--- backend/config/models.py
+++ backend/config/models.py
@@ -1,19 +1,5 @@
 from django.db import models
-from dataprocess.models import CollectTarget, Platform
-import datetime
-
-
-class PlatformTargetItem(models.Model):
-    platform = models.ForeignKey(to=Platform, on_delete=models.CASCADE)
-    target_name = models.TextField(default="")
-    target_type = models.TextField(default="int")
-    xpath = models.TextField(default="", blank=True)
-    create_dt = models.DateTimeField(auto_now_add=True)
-    update_dt = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "platform_target_item"
-
+from dataprocess.models import CollectTarget
 
 class CollectTargetItem(models.Model):
     collect_target = models.ForeignKey(to=CollectTarget, on_delete=models.CASCADE)
